4abbf14b5d92378a3168461be581173e5bc23c16
katerynapidhorna <katerynapidhorna@gmail.com>
2020-10-27 12:16:01 +0100

    add fetch & render data from the server

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 // const folder = require("./uploadedImages");
-(() => {
-  console.log("loaded");
+(async () => {
+  const container = document.getElementById("content");
+  const res = await fetch("http://localhost:4000/");
+  const data = await res.json();
+  const render =
+    data &&
+    data.map((elem) => {
+      return `<img src='${elem}'/>`;
+    });
+  if (!render) {
+    container.innerHTML = render.join(" ");
+  } else {
+    container.innerHTML = "no images";
+  }
 })();
